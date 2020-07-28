0b195a34b1ae4e9a4ef71e385381c730ca9a5578
Mariella Saviola <msaviola@sandiego.edu>
2020-07-28 14:03:33 -0700

    questions page with working timer

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
 
 var playButton = document.querySelector("#start");
-
+// var startQuiz = document.querySelector("#button");
 var secondsDisplay = document.querySelector("#seconds");
+
 
 
 var totalSeconds = 60;
 var secondsElapsed = 0;
-var interval;
-
-/* One thing to distinguish here is that not all functions are created equal.
-   Some functions just change settings, some functions just call other functions,
-   some functions just format strings or numbers, etc. */
+//var interval;
 
 // This launches the app by calling setTime() and renderTime()
 
@@ -31,14 +28,10 @@ function getFormattedSeconds() {
   return formattedSeconds;
 }
 
-/* This function retrieves the values from the html input elements; Sort of
-   getting run in the background, it sets the totalSeconds variable which
-   is used in getFormattedMinutes/Seconds() and the renderTime() function.
-   It essentially resets our timer */
 function setTime() {
   
-  totalSeconds = 10;
-}
+    totalSeconds = 60;
+  }
 
 // This function does 2 things. displays the time and checks to see if time is up.
 function renderTime() {
@@ -55,7 +48,9 @@ function renderTime() {
 // This function is where the "time" aspect of the timer runs
 // Notice no settings are changed other than to increment the secondsElapsed var
 function startTimer() {
-  setTime();
+    console.log("started Timer");
+    window.open("question.html");
+    setTime();
 
   // We only want to start the timer if totalSeconds is > 0
   if (totalSeconds > 0) {
@@ -72,16 +67,19 @@ function startTimer() {
 
 
 
-/* This function stops the interval and also resets secondsElapsed
-   and calls "setTime()" which effectively reset the timer
-   to the input selections workMinutesInput.value and restMinutesInput.value */
+/* This function stops the interval and also resets secondsElapsed*/
+  
 function stopTimer() {
   secondsElapsed = 0;
   alert("Time is Up!");
+  alert("Your score is:");
 
 }
 
 
-playButton.addEventListener("click", startTimer);
+console.log("we are in script");
+
+startTimer();
+
 
 
